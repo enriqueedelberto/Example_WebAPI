a8c3fbd5a13c1c2a57f9e5fadc6c933bcb8419cd
Edelberto Enrique Reyes <edelbertoenriquereyes@gmail.com>
2019-08-01 23:49:50 -0500

    Modificar definición

--- Database/Tables/Tasks _SQLServer.sql
+++ Database/Tables/Tasks _SQLServer.sql
@@ -1,9 +1,7 @@
-DROP TABLE IF EXISTS `Tasks`;
-
-
-CREATE TABLE EXISTS ToDoList.Tasks
+create schema ToDoList
+CREATE TABLE ToDoList.Tasks
   (
-    id_task INT PRIMARY KEY IDENTITY (1, 1) PRIMARY KEY,
+    id_task INT PRIMARY KEY IDENTITY (1, 1),
 	cd_task VARCHAR(255) NOT NULL,
 	title_task VARCHAR(255) NOT NULL,
 	ds_task VARCHAR(255) NULL,
@@ -14,10 +12,3 @@ CREATE TABLE EXISTS ToDoList.Tasks
 	
 	
   );
-  
-  
-  
-  
-  INSERT INTO `tasks` (`id_task`, `title_task`, `ds_task`, `status_task`, `id_user`, `createOnDate`, `lastModifiedOnDate`) 
-               VALUES 
-			   (NULL, 'Task 1', 'Crear una base de datos en Mysql', 'IN_PROGRESS', '1', '2019-07-09', '2019-07-09');
